109a617049ffd938b729a695743f5b8ba5638861
Nick Desaulniers <wangstabill@snet.net>
2013-06-25 01:48:53 -0700

    fix scroll bar issue

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 $(function () {
+  var mapShown = false;
+
   var resizeMap = function () {
-    // 50 is a magic constant to account for padding/margin -- temporary
-    $("#map").height($(window).height() - $("#navbar").height() - 50);
+    if (mapShown) {
+      // 50 is a magic constant to account for padding/margin -- temporary
+      $("#map").height($(window).height() - $("#navbar").height() - 50);
+    } else {
+      $("#map").hide();
+    }
   };
 
   var showMap = function (evt) {
     evt.preventDefault();
+    mapShown = true;
     $("#navbtn").trigger('click');
     $("#map").css('position', 'static').show();
     $("#logo").hide();
@@ -14,6 +21,7 @@ $(function () {
 
   var showLogo = function (evt) {
     evt.preventDefault();
+    mapShown = false;
     $("#map").hide();
     $("#logo").show();
   };
