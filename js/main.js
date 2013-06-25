4b25180e6f043bc571ece97cf83fb8849cf9c9c6
Nick Desaulniers <wangstabill@snet.net>
2013-06-25 01:50:39 -0700

    single quote all string literals

--- js/main.js
+++ js/main.js
@@ -4,32 +4,32 @@ $(function () {
   var resizeMap = function () {
     if (mapShown) {
       // 50 is a magic constant to account for padding/margin -- temporary
-      $("#map").height($(window).height() - $("#navbar").height() - 50);
+      $('#map').height($(window).height() - $('#navbar').height() - 50);
     } else {
-      $("#map").hide();
+      $('#map').hide();
     }
   };
 
   var showMap = function (evt) {
     evt.preventDefault();
     mapShown = true;
-    $("#navbtn").trigger('click');
-    $("#map").css('position', 'static').show();
-    $("#logo").hide();
+    $('#navbtn').trigger('click');
+    $('#map').css('position', 'static').show();
+    $('#logo').hide();
     resizeMap();
   };
 
   var showLogo = function (evt) {
     evt.preventDefault();
     mapShown = false;
-    $("#map").hide();
-    $("#logo").show();
+    $('#map').hide();
+    $('#logo').show();
   };
 
   // nav bar changes size dynamically over time; easiest to just update
   // the map continually
   setInterval(resizeMap, 200);
 
-  $("#js-map").on('click', showMap);
-  $("#js-index").on('click', showLogo);
+  $('#js-map').on('click', showMap);
+  $('#js-index').on('click', showLogo);
 });
