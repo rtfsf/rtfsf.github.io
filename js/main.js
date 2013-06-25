7a60f562b8f08b71dc092b1c308a6c59d224b65e
Nick Desaulniers <wangstabill@snet.net>
2013-06-25 01:28:17 -0700

    prevent some defaults

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-$(function() {
-  var resizeMap = function() {
+$(function () {
+  var resizeMap = function () {
     // 50 is a magic constant to account for padding/margin -- temporary
     $("#map").height($(window).height() - $("#navbar").height() - 50);
   };
 
-  var showMap = function() {
+  var showMap = function (evt) {
+    evt.preventDefault();
     $("#navbtn").trigger('click');
     $("#map").css('position', 'static');
     $("#logo").hide();
     resizeMap();
   };
 
-  var showLogo = function () {
+  var showLogo = function (evt) {
+    evt.preventDefault();
     $("#map").hide();
     $("#logo").show();
   };
